fcf720374c11e77391808cd85f7e567b69e48782
DillTheBeast <68816809+DillTheBeast@users.noreply.github.com>
2022-09-04 19:45:48 -0400

    t

---
+++ src/typingTest/Controller.java
@@ -0,0 +1,14 @@
+package typingTest;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Label label;
+
+    public void initialize() {
+
+    }
+}
