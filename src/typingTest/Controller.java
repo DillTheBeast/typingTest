d715de47820b62f5b2a4504fc4b16a361b0b7b66
DillTheBeast <dillonmaltese@gmail.com>
2022-09-06 07:53:18 -0400

    t

--- src/typingTest/Controller.java
+++ src/typingTest/Controller.java
@@ -2,11 +2,27 @@ package typingTest;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 public class Controller {
 
     @FXML
-    private Label label;
+    private Text wpm;
+
+    @FXML
+    private Text seconds;
+
+    @FXML
+    private Text accuracy;
+
+    @FXML
+    private Text currentWord;
+
+    @FXML
+    private Text nextWord;
+
+    @FXML
+    private Text inputWord;
 
     public void initialize() {
 
