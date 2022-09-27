05a1b62f6fa9d3620225280c35576935ae6e066b
DillTheBeast <dillonmaltese@gmail.com>
2022-09-27 08:45:28 -0400

    g

--- src/typingTest/Main.java
+++ src/typingTest/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("typingTest.fxml"));
         primaryStage.setTitle("Typing Test");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("unnamed.png")));
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
